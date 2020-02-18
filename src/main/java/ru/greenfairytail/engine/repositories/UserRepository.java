1113a47d843cac3e2cb34eb6e2ebf23bf829f203
ibelogolovy <ilya.whitehead@gmail.com>
2020-02-18 21:40:02 +0300

    dep

--- src/main/java/ru/greenfairytail/engine/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.greenfairytail.engine.repositories;
-
-import ru.greenfairytail.engine.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
